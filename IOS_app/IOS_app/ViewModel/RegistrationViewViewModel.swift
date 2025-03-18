c0641336dbeee3518d070db833bee3ba514c337a
KonarAndriiana <144444301+KonarAndriiana@users.noreply.github.com>
2025-03-18 21:35:14 +0100

    Add validate function
    Create function to validate fields, email correct format  and password length  . Add that function to registration function as a guard

--- IOS_app/IOS_app/ViewModel/RegistrationViewViewModel.swift
+++ IOS_app/IOS_app/ViewModel/RegistrationViewViewModel.swift
@@ -15,12 +15,30 @@ class RegistrationViewViewModel: ObservableObject {
         
     }
     
-    func registrate() {
-        
+    func registration() {
+        guard validate() else {
+            return
+        }
     }
     
     
-    private func validate() {
+    private func validate() -> Bool {
+        guard !email.trimmingCharacters(in: .whitespaces).isEmpty,
+              !password.trimmingCharacters(in: .whitespaces).isEmpty else {
+            return false
+        }
         
+        guard email.contains("@")  && email.contains(".") else {
+            return false
+        }
+        
+        guard email.count >= 6 else {
+            return false
+        }
+        
+        // just for console log
+        print("Trying to registrate")
+        
+        return true
     }
 }
