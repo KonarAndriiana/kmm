00d168c19c6fe90002fa1f9d3312fe81f3fbfa00
KonarAndriiana <144444301+KonarAndriiana@users.noreply.github.com>
2025-03-18 21:24:09 +0100

    Create RegistrationView VM
    Add  variables  to new ViewModel

---
+++ IOS_app/IOS_app/ViewModel/RegistrationViewViewModel.swift
@@ -0,0 +1,26 @@
+//
+//  RegistrationViewViewModel.swift
+//  IOS_app
+//
+//  Created by Andriiana Konar on 18/03/2025.
+//
+
+import Foundation
+
+class RegistrationViewViewModel: ObservableObject {
+    @Published var email = " "
+    @Published var password = " "
+    
+    init() {
+        
+    }
+    
+    func registrate() {
+        
+    }
+    
+    
+    private func validate() {
+        
+    }
+}
