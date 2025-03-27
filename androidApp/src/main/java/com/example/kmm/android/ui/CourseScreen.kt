647454830ffd29bfe683a08504760c94cba91af5
SzilardVysoky <szilard.vysoky@kosickaakademia.sk>
2025-03-27 09:14:21 +0100

    add scroll to CourseScreen

--- androidApp/src/main/java/com/example/kmm/android/ui/CourseScreen.kt
+++ androidApp/src/main/java/com/example/kmm/android/ui/CourseScreen.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
@@ -30,6 +32,7 @@ fun CourseScreen(navController: NavController, courseId: Int? = null) {
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .verticalScroll(rememberScrollState())
             .background(Color.White)
             .padding(16.dp)
     ) {
