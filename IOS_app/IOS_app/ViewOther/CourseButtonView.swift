e8d359b984048ba1625e8f6d23074cd3ff4a30f9
KonarAndriiana <144444301+KonarAndriiana@users.noreply.github.com>
2025-03-31 18:59:30 +0200

    Add CourseButtonView
    Reusable course selection button with title and subtitle

---
+++ IOS_app/IOS_app/ViewOther/CourseButtonView.swift
@@ -0,0 +1,41 @@
+//
+//  CourseButtonView.swift
+//  IOS_app
+//
+//  Created by Andriiana Konar on 28/03/2025.
+//
+import SwiftUI
+
+struct CourseButtonView: View {
+    
+    let title: String
+    let subtitle: String
+    let bg_color: Color
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 10)
+                .foregroundColor(bg_color)
+            
+            VStack(alignment: .leading) {
+                Text(title)
+                    .font(.title)
+                    .fontWeight(.medium)
+                    .lineLimit(1)
+                
+                Text(subtitle)
+                    .padding(.top, 2.0)
+            }
+            .padding()
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    CourseButtonView(
+        title: "Course",
+        subtitle: "Ready to learn? Choose your course and start your journey! Select a topic that interests you and dive into a new adventure in coding.",
+        bg_color: .gray
+    )
+}
