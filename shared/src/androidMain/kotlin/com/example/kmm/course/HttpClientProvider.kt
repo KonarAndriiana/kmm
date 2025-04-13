016bd258431ca338aad8577cb618c4e8f54410eb
KonarAndriiana <144444301+KonarAndriiana@users.noreply.github.com>
2025-04-13 17:51:15 +0200

    Update  HttpClientProvider in Android

--- shared/src/androidMain/kotlin/com/example/kmm/course/HttpClientProvider.kt
+++ shared/src/androidMain/kotlin/com/example/kmm/course/HttpClientProvider.kt
@@ -1,16 +1,20 @@
+// shared/src/androidMain/kotlin/com/example/kmm/course/HttpClientProvider.kt
 package com.example.kmm.course
 
-import io.ktor.client.HttpClient
-import io.ktor.client.engine.okhttp.OkHttp
-import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.serialization.kotlinx.json.json
+import io.ktor.client.*
+import io.ktor.client.engine.okhttp.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.serialization.kotlinx.json.*
 import kotlinx.serialization.json.Json
 
 actual class HttpClientProvider {
-    actual fun createClient(): HttpClient {
+    actual fun getClient(): HttpClient {
         return HttpClient(OkHttp) {
             install(ContentNegotiation) {
-                json(Json { ignoreUnknownKeys = true })
+                json(Json {
+                    ignoreUnknownKeys = true
+                    isLenient = true
+                })
             }
         }
     }
