2a1b0ac69cc59f0fb16d00da67b7e562282ef8f0
KonarAndriiana <144444301+KonarAndriiana@users.noreply.github.com>
2025-04-22 09:48:44 +0200

    Update RegitrationView

--- iosApp/iosApp/View/Pages/RegistrationView.swift
+++ iosApp/iosApp/View/Pages/RegistrationView.swift
@@ -14,78 +14,121 @@ struct RegistrationView: View {
     @State private var showToast = false
     
     var body: some View {
-        VStack {
-            VStack{
-                Text("Create Account")
-                    .accessibilityIdentifier("create_acc_text")
-            }
-            .padding(.top, 100)
-            
-            Form {
-                TextFieldView(placeholder: "First name", text: $viewModel.firstName)
-                    .autocorrectionDisabled()
-                    .accessibilityIdentifier("first_name_input")
+        NavigationView{
+            ZStack{
+                Image("backgroundImage")
+                    .resizable()
+                    .scaledToFill()
+                    .ignoresSafeArea()
+                    .accessibilityIdentifier("background_image")
                 
-                TextFieldView(placeholder: "Last name", text: $viewModel.lastName)
-                    .autocorrectionDisabled()
-                    .accessibilityIdentifier("last_name_input")
-                
-                TextFieldView(placeholder: "Email", text: $viewModel.email)
-                    .autocapitalization(.none)
-                    .autocorrectionDisabled()
-                    .accessibilityIdentifier("email_input")
-                
-                TextFieldView(placeholder: "Password", text: $viewModel.password, isSecure: true)
-                    .autocapitalization(.none)
-                    .autocorrectionDisabled()
-                    .accessibilityIdentifier("password_input")
-                
-                TextFieldView(placeholder: "Confirm Password", text: $viewModel.confirmPassword, isSecure: true)
-                    .autocapitalization(.none)
-                    .autocorrectionDisabled()
-                    .accessibilityIdentifier("confirm_password")
-                
-                ButtonView(title: "Sign Up") {
-                    viewModel.registration()
-                }
-                .disabled(viewModel.password != viewModel.confirmPassword || viewModel.password.isEmpty || viewModel.confirmPassword.isEmpty)
-                .opacity((viewModel.password != viewModel.confirmPassword || viewModel.password.isEmpty || viewModel.confirmPassword.isEmpty) ? 0.5 : 1.0)
-                .accessibilityIdentifier("sign_up_btn")
-            }
-            .padding(.top, 100)
-        }
-        .overlay(
-            VStack {
-                if showToast {
-                    HStack {
-                        Text(viewModel.errorMessage)
-                            .font(.body)
-                            .foregroundColor(.white)
-                            .padding()
-                            .frame(maxWidth: .infinity)
-                            .background(Color.gray)
-                            .cornerRadius(10)
-                            .shadow(radius: 10)
+                VStack {
+                    VStack{
+                        Text("Create account")
+                            .font(.system(size: 35))
+                            .multilineTextAlignment(.center)
+                            .foregroundColor(Color.white)
+                            .bold()
+                            .accessibilityIdentifier("create_acc_text")
                     }
-                    .padding(.horizontal)
-                    .frame(maxWidth: 400)
-                    .transition(.opacity)
-                    .zIndex(1)
-                    .onAppear {
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
-                            withAnimation {
-                                showToast = false
+                    .padding(.bottom, 150)
+                    
+                    //fields
+                    VStack(spacing: 50) {
+                        TextFieldView(placeholder: "first name", text: $viewModel.firstName)
+                            .frame(width: 440.0, height: 25.0)
+                            .autocorrectionDisabled()
+                            .accessibilityIdentifier("first_name_input")
+                        
+                        TextFieldView(placeholder: "last name", text: $viewModel.lastName)
+                            .frame(width: 440.0, height: 25.0)
+                            .autocorrectionDisabled()
+                            .accessibilityIdentifier("last_name_input")
+                        
+                        TextFieldView(placeholder: "email", text: $viewModel.email)
+                            .frame(width: 440.0, height: 25.0)
+                            .autocapitalization(.none)
+                            .autocorrectionDisabled()
+                            .accessibilityIdentifier("email_input")
+                        
+                        TextFieldView(placeholder: "password", text: $viewModel.password, isSecure: true)
+                            .frame(width: 440.0, height: 25.0)
+                            .autocapitalization(.none)
+                            .autocorrectionDisabled()
+                            .accessibilityIdentifier("password_input")
+                        
+                        TextFieldView(placeholder: "confirm password", text: $viewModel.confirmPassword, isSecure: true)
+                            .frame(width: 440.0, height: 25.0)
+                            .autocapitalization(.none)
+                            .autocorrectionDisabled()
+                            .accessibilityIdentifier("confirm_password")
+                    }
+                    .padding(.top, 20)
+                    
+                    
+                    //Sign In
+                    VStack(spacing: 30) {
+                        ButtonView(title: "Sign Up") {
+                            viewModel.registration()
+                        }
+                        .disabled(viewModel.password != viewModel.confirmPassword || viewModel.password.isEmpty || viewModel.confirmPassword.isEmpty)
+                        .opacity((viewModel.password != viewModel.confirmPassword || viewModel.password.isEmpty || viewModel.confirmPassword.isEmpty) ? 0.5 : 1.0)
+                        .frame(width: 150.0, height: 60.0)
+                        .accessibilityIdentifier("sign_up_btn")
+                        
+                        HStack {
+                            Text("Already have an account?")
+                                .font(.system(size: 15))
+                                .foregroundColor(Color.white)
+                                .accessibilityIdentifier("sign_up_text")
+                                
+                            
+                            NavigationLink(destination: LoginView()) {
+                                Text("Log In")
+                                    .font(.system(size: 15))
+                                    .foregroundColor(.white)
+                                    .underline()
+                                    .bold()
                             }
+                            .accessibilityIdentifier("log_in_redirect")
                         }
                     }
+                    .padding(.top, 50)
                 }
-                Spacer()
-            }
-                .accessibilityIdentifier("error_msg")
-        )
-        .onChange(of: viewModel.errorMessage) { newError in
-            if !newError.isEmpty {
-                showToast = true
+                .overlay(
+                    VStack {
+                        if showToast {
+                            HStack {
+                                Text(viewModel.errorMessage)
+                                    .font(.body)
+                                    .foregroundColor(.white)
+                                    .padding()
+                                    .frame(maxWidth: .infinity)
+                                    .background(Color.gray)
+                                    .cornerRadius(10)
+                                    .shadow(radius: 10)
+                            }
+                            .padding(.horizontal)
+                            .frame(maxWidth: 400)
+                            .transition(.opacity)
+                            .zIndex(1)
+                            .onAppear {
+                                DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+                                    withAnimation {
+                                        showToast = false
+                                    }
+                                }
+                            }
+                        }
+                        Spacer()
+                    }
+                        .accessibilityIdentifier("error_msg")
+                )
+                .onChange(of: viewModel.errorMessage) { newError in
+                    if !newError.isEmpty {
+                        showToast = true
+                    }
+                }
             }
         }
     }
