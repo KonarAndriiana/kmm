100ecaa1077db5702b7fd7f7359f4c7de332a794
KonarAndriiana <144444301+KonarAndriiana@users.noreply.github.com>
2025-04-28 12:17:15 +0200

    ..

--- iosApp/iosApp/View/Pages/RegistrationView.swift
+++ iosApp/iosApp/View/Pages/RegistrationView.swift
@@ -23,11 +23,69 @@ struct RegistrationView: View {
                     .accessibilityIdentifier("background_image")
                 
                 VStack(spacing: 150){
-                    RegistrationHeaderView()
+                    Text("Create account")
+                        .font(.system(size: 35))
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(Color.white)
+                        .bold()
+                        .accessibilityIdentifier("create_acc_text")
                     
-                    RegistrationInputView()
+                    VStack(spacing: 50) {
+                        TextFieldView(placeholder: "first name", text: $viewModel.firstName)
+                            .frame(width: 440.0, height: 20.0)
+                            .autocorrectionDisabled()
+                            .accessibilityIdentifier("first_name_input")
+                        
+                        TextFieldView(placeholder: "last name", text: $viewModel.lastName)
+                            .frame(width: 440.0, height: 20.0)
+                            .autocorrectionDisabled()
+                            .accessibilityIdentifier("last_name_input")
+                        
+                        TextFieldView(placeholder: "email", text: $viewModel.email)
+                            .frame(width: 440.0, height: 20.0)
+                            .autocapitalization(.none)
+                            .autocorrectionDisabled()
+                            .accessibilityIdentifier("email_input")
+                        
+                        TextFieldView(placeholder: "password", text: $viewModel.password, isSecure: true)
+                            .frame(width: 440.0, height: 20.0)
+                            .autocapitalization(.none)
+                            .autocorrectionDisabled()
+                            .accessibilityIdentifier("password_input")
+                        
+                        TextFieldView(placeholder: "confirm password", text: $viewModel.confirmPassword, isSecure: true)
+                            .frame(width: 440.0, height: 20.0)
+                            .autocapitalization(.none)
+                            .autocorrectionDisabled()
+                            .accessibilityIdentifier("confirm_password")
+                    }
                     
-                    RegistrationBttnView()
+                    VStack(spacing: 30) {
+                        ButtonView(title: "Sign Up") {
+                            viewModel.registration()
+                        }
+                        .disabled(viewModel.password != viewModel.confirmPassword || viewModel.password.isEmpty || viewModel.confirmPassword.isEmpty)
+                        .opacity((viewModel.password != viewModel.confirmPassword || viewModel.password.isEmpty || viewModel.confirmPassword.isEmpty) ? 0.5 : 1.0)
+                        .frame(width: 150.0, height: 60.0)
+                        .accessibilityIdentifier("sign_up_btn")
+                        
+                        HStack {
+                            Text("Already have an account?")
+                                .font(.system(size: 15))
+                                .foregroundColor(Color.white)
+                                .accessibilityIdentifier("sign_up_text")
+                                
+                            
+                            NavigationLink(destination: LoginView()) {
+                                Text("Log In")
+                                    .font(.system(size: 15))
+                                    .foregroundColor(.white)
+                                    .underline()
+                                    .bold()
+                            }
+                            .accessibilityIdentifier("log_in_redirect")
+                        }
+                    }
                 }
                 .overlay(
                     VStack {
