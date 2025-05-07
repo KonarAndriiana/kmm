fe950223217cdeea8e4d8e2202cb42fb64f1d9a9
KonarAndriiana <144444301+KonarAndriiana@users.noreply.github.com>
2025-05-07 13:15:01 +0200

    Add new Views

---
+++ iosApp/iosApp/View/Pages/CourseView.swift
@@ -0,0 +1,19 @@
+//
+//  CourseView.swift
+//  iosApp
+//
+//  Created by Andriiana Konar on 07/05/2025.
+//  Copyright © 2025 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct CourseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CourseView()
+}
