0d06474a926ea9d0eb13d77f565f0178985c8b09
KonarAndriiana <144444301+KonarAndriiana@users.noreply.github.com>
2025-05-13 16:02:22 +0200

    Update CourseView

--- iosApp/iosApp/View/Components/CourseView.swift
+++ iosApp/iosApp/View/Components/CourseView.swift
@@ -20,11 +20,43 @@ struct CourseView: View {
     
     var coursesList : [Courses] = [
         .init(name: "Java", specification: "Backend", level: "Beginner", photo: "bg_1"),
-        .init(name: "SQL", specification: "Database", level: "Intermediate", photo: "bg_2"),
-        .init(name: "IT security", specification: "Security", level: "Intermediate" , photo: "bg_3")
+        .init(name: "SQL", specification: "Database", level: "Intermediate", photo: "bg_4"),
+        .init(name: "IT security", specification: "Security", level: "Intermediate" , photo: "bg_0")
     ]
     
     var body: some View {
+        
+        VStack(spacing: 20) {
+            
+            VStack(alignment: .leading , spacing: 10) {
+                HStack {
+                    Text("Course")
+                        .font(.system(size: 30))
+                        .fontWeight(.bold)
+                    
+                    Spacer()
+                    
+                    Button{
+                        //code
+                    } label: {
+                        Text("see all")
+                            .font(.system(size: 15))
+                            .fontWeight(.light)
+                            .foregroundStyle(.gray)
+                            .underline()
+                    }
+                }
+                
+                
+                Text("""
+                     Ready to learn? Choose your course and start your journey! Select a topic that interests you and dive into a new adventure in coding
+                     """)
+                .font(.system(size: 20))
+                .fontWeight(.regular)
+            }
+            .padding()
+        
+            
             ScrollView(.horizontal, showsIndicators: false) {
                 HStack(spacing: 16) {
                     ForEach(coursesList) { course in
@@ -34,6 +66,8 @@ struct CourseView: View {
                 .padding(.horizontal)
             }
         }
+        
+    }
 }
 
 
